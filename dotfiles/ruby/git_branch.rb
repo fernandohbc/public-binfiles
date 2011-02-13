587d3aeab1baa80f2b8d702f5b920621675dbb45
Fernando Cardoso <fernandohbc@gmail.com>
2011-02-13 19:00:08 -0300

    Removed ruby and RoR references from git_branch.rb

--- dotfiles/ruby/git_branch.rb
+++ dotfiles/ruby/git_branch.rb
@@ -7,11 +7,8 @@ blue   = "\033[0;34m"
 current_branch = `git branch 2>/dev/null`.lines.grep(/^\*/).first
 
 if current_branch
-  rails_version = ENV["GEM_HOME"] =~ /rails2/ ? "#{yellow}[rails 2]" : "#{green}[rails 3]"
-  ruby_version = "#{`rvm-prompt i v`.split.first}"
-
   branch_name = current_branch.gsub(/^\*\s*/,'').strip
   color = branch_name  =~ /master/ ? green : red
-  puts "#{color}[#{branch_name}] #{blue}[#{ruby_version}] #{rails_version}#{white}" unless current_branch.empty?
+  puts "#{color}[branch: #{branch_name}]" unless current_branch.empty?
 end
 
